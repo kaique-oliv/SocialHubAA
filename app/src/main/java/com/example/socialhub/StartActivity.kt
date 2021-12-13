e53af6ee52a003ccd4b3b32d3b4007ee964e133d
Kaique De Oliveira <kaique.oliveira@prestadores.uhgbrasil.com.br>
2021-12-12 21:44:34 -0300

    arquivos

--- app/src/main/java/com/example/socialhub/StartActivity.kt
+++ app/src/main/java/com/example/socialhub/StartActivity.kt
@@ -1,11 +1,9 @@
-
-
 package com.example.socialhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
